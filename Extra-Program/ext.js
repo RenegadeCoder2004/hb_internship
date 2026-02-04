e9682fcb6d6531eae71031b53ddb4b1751c3426d
RenegadeCoder2004 <renegadeyt01@gmail.com>
2026-02-04 09:01:23 +0530

    Extra Programs

---
+++ Extra-Program/ext.js
@@ -0,0 +1,14 @@
+let a = 2 + 2;
+switch(a){
+    case 3:
+        alert('Too Small');
+        break;
+    case 4:
+        alert('Exactly');
+        break;
+    case 5:
+        alert('Too Big');
+        break;
+    default:
+        alert("I don't know such values");
+}
